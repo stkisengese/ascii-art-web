8cb3ed8e9c8580712cfaef7e5df095f02598a947
skisenge <skisengese@gmail.com>
2024-07-16 15:22:32 +0300

    fix: error handling and file path issues.

--- handlers/indexhandler.go
+++ handlers/indexhandler.go
@@ -1,13 +1,26 @@
 package handlers
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
 )
 
 // IndexHandler renders an HTML template for the root path.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	// Handle non-root paths
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Handle only Get method for the root path
+	if r.Method != http.MethodGet {
+		http.Error(w, "Error 405: Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Render the template for the root path
+	tmpl := template.Must(template.ParseFiles("../templates/index.html"))
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, "Error 500: Internal server error", http.StatusInternalServerError)
