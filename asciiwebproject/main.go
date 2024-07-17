507df6bd2e15452fd4832dc369d9c2b0fe18ad30
mombewa <ombewamaurice79@gmail.com>
2024-07-17 17:03:27 +0300

    style: updated comments.

--- asciiwebproject/main.go
+++ asciiwebproject/main.go
@@ -9,20 +9,25 @@ import (
 )
 
 func main() {
+	// Set up HTTP handlers
 	http.HandleFunc("/", handler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 
+	// Determine the port in use
 	port := "8080"
 	if len(os.Args) > 1 {
 		port = os.Args[1]
 	}
 	fmt.Printf("Starting server on port %s...\n", port)
+
+	// start the server
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
 
+// Handle different url paths.
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 
